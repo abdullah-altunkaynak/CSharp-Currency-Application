08694426a5adee61a53b4243239ccd5189ab3ce6
Abdullah ALTUNKAYNAK <58576152+abdullah-altunkaynak@users.noreply.github.com>
2021-09-21 19:06:49 +0300

    Add files via upload

---
+++ DovizUygulamasi/Classes/CurrencyData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DovizUygulamasi
+{
+    // her verinin dolarla karşılığı bilinmeli
+    /* Çapraz kur hesaplamasında 3 birim olmalıdır. Örenğin euro, dolar ve tl. Burada euro ve tl nin dolarla karşılığı bilinirse
+       euro ve doların birbirleri ile karşılığı bilinebilir. Şöyleki; Euro/Dolar ve Dolar/Tl değeri bilinirse işlem bizim için 
+       daha kolay olur. Bunun için işlem yapılacağı zaman x/usd ve usd/x formatına dönüşüm yapıp sınıfta metotu kullanıyoruz.
+      */
+    public class Currency
+    {
+        public string Name { get; }
+        public double ForexBuying { get; }
+
+        public Currency(string name, double forexBuying)
+        {
+            Name = name;
+            ForexBuying = forexBuying;
+        }
+        public double CalculateForExchange(double forexBuying) // Bu USD/x, USD/x için
+        {
+            forexBuying = 1 / forexBuying;
+            return forexBuying;
+        }
+        public double CalculateCurrency(double from, double to)// Bu x/USD , USD/x için  USD/x ve x/USD
+        {                                                                                // tl ve euro
+            to = from * to;
+            return to;
+        }
+    }
+    // Main Ekranındaki döviz verilerini buyform ekranında kullanabilmek için bu sınıf olmalı.
+    // Bu sınıfı oluşturmadan önce direk olarak label.text'leri çekerek işlem yapıyordum bu da bana güvenlik açığı oluşturuyordu.
+    public class MainScreenCurrency
+    {
+        public string fromCurrencyName { get; set; }
+        public string toCurrencyName { get; set; }
+        public double currencyBuying { get; set; }
+        public double currencySelling { get; set; }
+    }
+}
---
+++ DovizUygulamasi/Forms/BuyForm.cs
@@ -0,0 +1,84 @@
+﻿using DovizUygulamasi.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DovizUygulamasi
+{
+    public partial class BuyForm : Form
+    {
+        public MainForm mainForm = new MainForm();
+        public bool buyorsell = false; // false = buy, true = sell
+        public BuyForm()
+        {
+            InitializeComponent();
+        }
+        private void BuyForm_Load(object sender, EventArgs e)
+        {
+            Button1.Enabled = true;
+            fromLabel.Text = CalculateExtensions.mainScreenCurrency.fromCurrencyName   ;//mainForm.fromComboBox.Text;
+            toLabel.Text = CalculateExtensions.mainScreenCurrency.toCurrencyName;
+            if (buyorsell) // eğer satma işlemi ise
+            {
+                Button1.BackColor = Color.Red;
+                Button1.Text = "SAT";
+                toTextBox.Text = (Convert.ToDouble(mainForm.buy1.Text) * Convert.ToDouble(fromTextBox.Text)).ToString();
+            }
+            else // eğer satın alma işlemi ise
+            {
+                Button1.BackColor = Color.LimeGreen;
+                Button1.Text = "SATIN AL";
+                toTextBox.Text = (Convert.ToDouble(mainForm.sell1.Text) * Convert.ToDouble(fromTextBox.Text)).ToString();
+            }
+            CalculateExtensions.toTextBox = toTextBox;
+        }
+        // Bu fonksiyonu fromtextbox'a girilen değerin sayı ya da "," olmasını sağlamak için kullanıyoruz.
+        bool SayiMi(string text)
+        {
+            foreach (char chr in text)
+            {
+                if (!Char.IsNumber(chr) && chr != ',')
+                    return false;
+            }
+            return true;
+        }
+        private void TextBoxValueChanged(object sender, EventArgs e)
+        {
+            toTextBox.Text = String.Empty;
+            if (fromTextBox.Text != string.Empty && SayiMi(fromTextBox.Text))
+            {
+                fromTextBox.ValueChanged(buyorsell, exchangeOfficeComboBox);                                                
+                Button1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Lütfen doğru değer giriniz.");
+                Button1.Enabled = false;
+            }               
+        }
+        private void ComboBoxValueChanged(object sender, EventArgs e)
+        {
+            fromTextBox.ValueChanged(buyorsell, exchangeOfficeComboBox);
+        }
+        // Bu fonksiyon ile cüzdan değerimizi kontrol ediyoruz yeterli mi değil mi.
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (toLabel.toLabelExtension(fromLabel, fromTextBox, toTextBox))
+            {
+                User.UpdateWallet();
+                User.AddTransactionTable(exchangeOfficeComboBox, fromLabel, toLabel, fromTextBox, toTextBox);
+                CalculateExtensions.MainScreenDataGridView();
+                User.ProfileScreenUpdate();
+            }
+            else
+                MessageBox.Show("Bakiye bilgileri kontrol edip tekrar deneyebilirsin.");
+        }
+    }
+}
